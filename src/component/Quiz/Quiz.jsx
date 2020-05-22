3ead7e810b794f76b32f7dfb205a154cde3fa202
Serghei Nuta <sergiu.nuta@gmail.com>
2020-05-22 18:26:32 +0100

    working validation

--- src/component/Quiz/Quiz.jsx
+++ src/component/Quiz/Quiz.jsx
@@ -1,10 +1,45 @@
 import React, { Component } from "react";
+import styles from "./Quiz.module.scss";
+
+import data from "./questions";
+
 
 export default class Quiz extends Component {
+    state = {
+        question: [],
+        answers: [],
+        correctAnswer: [],
+        result: true
+    }
+
+    checked = (event) => {
+        event.target.value === this.state.correctAnswer ? alert("Correct") : alert("Incorrect");
+    }
+
+    componentDidMount() {
+        const result = data.map(doc => {
+            this.setState({
+                question: doc.question,
+                answers: doc.answers,
+                correctAnswer: doc.correctAnswer
+
+            })
+        })
+    }
+
     render() {
-        return(
+        const answerData = this.state.answers;
+        return (
             <>
-            Quiz
+                <div className={styles.quiz}>
+                    <h1>React Quiz</h1>
+                    <p>{this.state.question}</p>
+                    {Object.entries(answerData).map((data, index) => {
+                        return (<section className={styles.row} key={index}>
+                            <input type="radio" name="firstAnswer" onChange={this.checked} value={data[1]} />
+                            <label>{data[1]}</label></section>)
+                    })}
+                </div>
             </>
         )
     }
---
+++ src/component/Quiz/questions.js
@@ -0,0 +1,16 @@
+
+
+const quiz = [
+    {
+        question: "Which city is the capital of UK?",
+        answers: {
+            1: "London",
+            2: "Paris",
+            3: "Berlin"
+        },
+        correctAnswer : "London"
+
+    }
+]
+
+export default quiz;
